da0f29a34fab9dfca53c4d945eb24ddb52f52627
Ethan Tuttle <tuttle.ethan@protonmail.com>
2023-08-27 19:32:18 -0400

    gpt summary

--- prs.sh
+++ prs.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Check if a token is provided
-if [[ -z "$1" ]]; then
-    echo "Usage: $0 <Your GitHub personal access token>"
+if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+    echo "Usage: $0 <Your GitHub personal access token> <Your OpenAI API Key>"
     exit 1
 fi
 
-TOKEN="$1"
 
-API_KEY=""
-API_URL="https://api.openai.com/v1/engines/text-davinci-004/completions"
+TOKEN="$1"
+API_KEY="$2"
+
+API_URL="https://api.openai.com/v1/chat/completions"  # Updated the URL
 
 # Variables
 OWNER="fedimint"
@@ -28,8 +29,8 @@ mkdir -p "$FOLDER_NAME"
 
 # Start the Table of Contents file
 TOC_FILE="${FOLDER_NAME}/TOC.md"
-echo "# Table of Contents for PRs from ${START_DATE} to ${END_DATE}" > $TOC_FILE
-echo "" >> $TOC_FILE
+echo "# Table of Contents for PRs from ${START_DATE} to ${END_DATE}" > "$TOC_FILE"
+echo "" >> "$TOC_FILE"
 
 # Fetch pull request URLs that were created in the last 7 days
 RESPONSE=$(curl -s -H "Authorization: token $TOKEN" \
@@ -48,54 +49,68 @@ while IFS= read -r line; do
     OUTFILE="${FOLDER_NAME}/${PR_FILE_NAME}"
     
     # Add to Table of Contents
-    echo "- [PR #${PR_NUM}: ${PR_TITLE}](${PR_FILE_NAME}) - [View on GitHub](${PR_LINK})" >> $TOC_FILE
+    echo "- [PR #${PR_NUM}: ${PR_TITLE}](${PR_FILE_NAME}) - [View on GitHub](${PR_LINK})" >> "$TOC_FILE"
     
     # Markdown Summary
-    echo "## SUMMARY" > $OUTFILE
-    echo "- **PR #${PR_NUM}:** [$PR_TITLE]($PR_LINK)" >> $OUTFILE
-    echo "" >> $OUTFILE
+    echo "## SUMMARY" > "$OUTFILE"
+    echo "- **PR #${PR_NUM}:** [$PR_TITLE]($PR_LINK)" >> "$OUTFILE"
+    echo "" >> "$OUTFILE"
 
     # Detailed Information
-    echo "## DETAILS" >> $OUTFILE
-    echo "### Description:" >> $OUTFILE
-    echo "$PR_DESC" >> $OUTFILE
-    echo "" >> $OUTFILE
+    echo "## DETAILS" >> "$OUTFILE"
+    echo "### Description:" >> "$OUTFILE"
+    echo "$PR_DESC" >> "$OUTFILE"
+    echo "" >> "$OUTFILE"
     
     # Fetch and dump comments
-    echo "### Comments:" >> $OUTFILE
+    echo "### Comments:" >> "$OUTFILE"
     COMMENTS=$(curl -s -H "Authorization: token $TOKEN" \
-         -H "Accept: application/vnd.github.v3+json" \
-         "https://api.github.com/repos/$OWNER/$REPO/issues/$PR_NUM/comments" | \
-    jq -r '.[] | "- **" + .user.login + ":** " + .body')
+        -H "Accept: application/vnd.github.v3+json" \
+        "https://api.github.com/repos/$OWNER/$REPO/issues/$PR_NUM/comments" | \
+    jq -r '.[] | select(.user.login != "codecov[bot]") | "- **" + .user.login + ":** " + .body')
     
-    TEXT_TO_SUMMARIZE=$(<"$PR_DESC")
     if [[ -z "$COMMENTS" ]]; then
-        echo "No comments for this PR." >> $OUTFILE
+        TEXT_TO_SUMMARIZE="$PR_DESC"
+        echo "No comments for this PR." >> "$OUTFILE"
     else
-        TEXT_TO_SUMMARIZE=$(<<"$COMMENTS")
-        echo "$COMMENTS" >> $OUTFILE
+        TEXT_TO_SUMMARIZE="${PR_DESC}\n${COMMENTS}"
+        echo "$COMMENTS" >> "$OUTFILE"
     fi
 
+    TEXT_TO_SUMMARIZE=$(echo "$TEXT_TO_SUMMARIZE" | sed 's/"/\\"/g')
+
+    # Use jq to safely construct the JSON string
+    JSON_SAFE_TEXT=$(echo "$TEXT_TO_SUMMARIZE" | jq -Rs .)
+
+    # Now, escape the double quotes using sed
+    ESCAPED_TEXT=$(echo "$JSON_SAFE_TEXT" | sed 's/"/\\"/g')
 
     # Construct the JSON payload
     PAYLOAD=$(cat <<EOF
-    {
-        "prompt": "Summarize the following text: \"$TEXT_TO_SUMMARIZE\"",
-        "max_tokens": 150
-    }
+{
+    "model": "gpt-3.5-turbo",
+    "messages": [{"role": "user", "content": "Summarize the following text: $ESCAPED_TEXT"}],
+    "temperature": 0.7
+}
 EOF
 )
+
+    echo $PAYLOAD
+
     # Make the API request
-    SUMMARY=$(curl -s \
+    API_RESPONSE=$(curl -s \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer $API_KEY" \
     -d "$PAYLOAD" \
-    "$API_URL" | jq -r .choices[0].text)
+    "$API_URL")
 
-    echo "GPT SUMMARY:" >> $OUTFILE
-    echo $SUMMARY >> $OUTFILE
-    
-    echo "" >> $OUTFILE
+    # Extract summary from the response
+    SUMMARY=$(echo "$API_RESPONSE")
+
+    echo "" >> "$OUTFILE"
+    echo "### GPT SUMMARY:" >> "$OUTFILE"
+    echo "$SUMMARY" | jq -r '.choices[0].message.content' >> "$OUTFILE"
+    echo "" >> "$OUTFILE"
 
 done <<< "$PR_ENTRIES"
 
